1fbd4a2c004bbe861998baa88e8e2219c17eaaec
Josiah Worcester <josiahw@gmail.com>
2015-04-05 18:26:48 -0500

    Simplify and optimize crc32 code.

--- crc32.c
+++ crc32.c
@@ -1,29 +1,15 @@
 /* crc32.c -- compute the CRC-32 of a data stream
  * Copyright (C) 1995-2006, 2010, 2011, 2012 Mark Adler
+ * Copyright (C) 2015 Josiah Worcester
  * For conditions of distribution and use, see copyright notice in zlib.h
  *
- * Thanks to Rodney Brown <rbrown64@csc.com.au> for his contribution of faster
- * CRC methods: exclusive-oring 32 bits of data at a time, and pre-computing
- * tables for updating the shift register in one step with three exclusive-ors
- * instead of four steps with four exclusive-ors.  This results in about a
- * factor of two increase in speed on a Power PC G4 (PPC7455) using gcc -O3.
  */
 
 /* @(#) $Id$ */
 
-#include <endian.h>
 #include <stdint.h>
 
 #include "zutil.h"
-
-/* Definitions for doing the crc four data bytes at a time. */
-#if BYTE_ORDER == LITTLE_ENDIAN
-static unsigned long crc32_little(unsigned long, const unsigned char *, unsigned int);
-#endif
-#if BYTE_ORDER == BIG_ENDIAN
-static unsigned long crc32_big(unsigned long, const unsigned char *, unsigned int);
-#endif
-#define TBLS 8
 
 /* Local functions for crc concatenation */
 static unsigned long gf2_matrix_times(unsigned long *mat, unsigned long vec);
@@ -43,120 +29,34 @@ const z_crc_t * ZEXPORT get_crc_table(void)
     return (const z_crc_t *)crc_table;
 }
 
-/* ========================================================================= */
-#define DO1 crc = crc_table[0][((int)crc ^ (*buf++)) & 0xff] ^ (crc >> 8)
-#define DO8 DO1; DO1; DO1; DO1; DO1; DO1; DO1; DO1
-
-/* ========================================================================= */
+/* =========================================================================
+ * Implement crc32 using Intel's "slicing by 8" algorithm. Significantly
+ * faster than most other common approachs on common CPUs at the time of
+ * this writing.
+ */
 unsigned long ZEXPORT crc32(unsigned long crc,
                             const unsigned char *buf,
                             unsigned int len)
 {
     if (buf == NULL) return 0UL;
 
-    if (sizeof(void *) == sizeof(ptrdiff_t)) {
-#if BYTE_ORDER == LITTLE_ENDIAN
-        return crc32_little(crc, buf, len);
-#endif
-#if BYTE_ORDER == BIG_ENDIAN
-        return crc32_big(crc, buf, len);
-#endif
-    }
     crc = crc ^ 0xffffffffUL;
-    while (len >= 8) {
-        DO8;
-        len -= 8;
+    for (; len > 0 && (uintptr_t)buf & 7; len--, buf++)
+        crc = crc_table[0][(crc & 0xff) ^ *buf] ^ (crc >> 8);
+    for (; len >= 8; len -= 8, buf += 8) {
+        crc = crc_table[7][(buf[0] ^ (crc      )) & 0xff]
+            ^ crc_table[6][(buf[1] ^ (crc >> 8 )) & 0xff]
+            ^ crc_table[5][(buf[2] ^ (crc >> 16)) & 0xff]
+            ^ crc_table[4][(buf[3] ^ (crc >> 24)) & 0xff]
+            ^ crc_table[3][buf[4]]
+            ^ crc_table[2][buf[5]]
+            ^ crc_table[1][buf[6]]
+            ^ crc_table[0][buf[7]];
     }
-    if (len) do {
-        DO1;
-    } while (--len);
+    for (; len > 0; len--, buf++)
+        crc = crc_table[0][(crc & 0xff) ^ *buf] ^ (crc >> 8);
     return crc ^ 0xffffffffUL;
 }
-
-#if BYTE_ORDER == LITTLE_ENDIAN
-/* ========================================================================= */
-#define DOLIT4 c ^= *buf4++; \
-        c = crc_table[3][c & 0xff] ^ crc_table[2][(c >> 8) & 0xff] ^ \
-            crc_table[1][(c >> 16) & 0xff] ^ crc_table[0][c >> 24]
-#define DOLIT32 DOLIT4; DOLIT4; DOLIT4; DOLIT4; DOLIT4; DOLIT4; DOLIT4; DOLIT4
-
-/* ========================================================================= */
-static unsigned long crc32_little(unsigned long crc,
-                                  const unsigned char *buf,
-                                  unsigned int len)
-{
-    z_crc_t c;
-    const z_crc_t *buf4;
-
-    c = (z_crc_t)crc;
-    c = ~c;
-    while (len && ((ptrdiff_t)buf & 3)) {
-        c = crc_table[0][(c ^ *buf++) & 0xff] ^ (c >> 8);
-        len--;
-    }
-
-    buf4 = (const z_crc_t *)(const void *)buf;
-    while (len >= 32) {
-        DOLIT32;
-        len -= 32;
-    }
-    while (len >= 4) {
-        DOLIT4;
-        len -= 4;
-    }
-    buf = (const unsigned char *)buf4;
-
-    if (len) do {
-        c = crc_table[0][(c ^ *buf++) & 0xff] ^ (c >> 8);
-    } while (--len);
-    c = ~c;
-    return (unsigned long)c;
-}
-#endif
-
-#if BYTE_ORDER == BIG_ENDIAN
-/* ========================================================================= */
-#define DOBIG4 c ^= *++buf4; \
-        c = crc_table[4][c & 0xff] ^ crc_table[5][(c >> 8) & 0xff] ^ \
-            crc_table[6][(c >> 16) & 0xff] ^ crc_table[7][c >> 24]
-#define DOBIG32 DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4
-
-/* ========================================================================= */
-static unsigned long crc32_big(unsigned long crc,
-                               const unsigned char *buf,
-                               unsigned int len)
-{
-    z_crc_t c;
-    const z_crc_t *buf4;
-
-    c = ZSWAP32((z_crc_t)crc);
-    c = ~c;
-    while (len && ((ptrdiff_t)buf & 3)) {
-        c = crc_table[4][(c >> 24) ^ *buf++] ^ (c << 8);
-        len--;
-    }
-
-    buf4 = (const z_crc_t *)(const void *)buf;
-    buf4--;
-    while (len >= 32) {
-        DOBIG32;
-        len -= 32;
-    }
-    while (len >= 4) {
-        DOBIG4;
-        len -= 4;
-    }
-    buf4++;
-    buf = (const unsigned char *)buf4;
-
-    if (len) do {
-        c = crc_table[4][(c >> 24) ^ *buf++] ^ (c << 8);
-    } while (--len);
-    c = ~c;
-    return (unsigned long)(ZSWAP32(c));
-}
-#endif
-
 
 #define GF2_DIM 32      /* dimension of GF(2) vectors (length of CRC) */
 
